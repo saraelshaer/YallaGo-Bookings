29b04445f6ffb996deb784a658d1efc8a422f99f
roqayalaa <rokalaa57@gmail.com>
2025-04-30 16:48:24 +0300

    Add AppDbContext

---
+++ YallaGo.DAL/Context/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using YallaGo.DAL.Models;
+namespace YallaGo.DAL
+{
+    public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Tour> Tours { get; set; }
+    public DbSet<Destination> Destinations { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Offer> Offers { get; set; }
+    public DbSet<OfferTour> OfferTours { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OfferTour>()
+            .HasKey(ot => new { ot.TourId, ot.OfferId });
+       }
+    }
+}
+
+
+
+
+
+
+
