447b8b38870b4ddab604b3c69c0eeb8687f7d0b3
saraelshaer <saraelshaer923@gmail.com>
2025-05-02 09:34:41 +0300

    Add AccountController

---
+++ YallaGo/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YallaGo.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
