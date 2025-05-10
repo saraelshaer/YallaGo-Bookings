66cdfccc825b21b264af6a9ded21ec866f6a1cbd
Alahraa-Abdallah <alzhraaabdallah261@gmail.com>
2025-05-10 22:58:19 +0300

    Add generic repository and unitOfWork

---
+++ YallaGo.DAL/Repositories/Repositories/GenericRepository.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace YallaGo.DAL.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+       
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void HardDelete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void SoftDelete(T entity)
+        {
+            var isActiveProp = entity.GetType().GetProperty("IsActive");
+            isActiveProp.SetValue(entity, false);
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+        public async Task<T> FindAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return await query.SingleOrDefaultAsync(criteria);
+        }
+
+        public async Task<IEnumerable<U>> SelectAsync<U>(Expression<Func<T, U>> expression , Expression<Func<T, bool>> criteria = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (criteria != null)
+                query = query.Where(criteria);
+
+            return await query.Select(expression).ToListAsync();
+        }
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>> criteria = null)
+        {
+            IQueryable<T> query = _dbSet.AsQueryable();
+            if (criteria != null)
+            {
+                query = query.Where(criteria);
+            }
+            return await query.CountAsync();
+        }
+
+        public async Task<bool> Exists(Expression<Func<T, bool>> criteria) => await _dbSet.AnyAsync(criteria);
+
+
+    }
+}
+
