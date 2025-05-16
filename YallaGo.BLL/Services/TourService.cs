6275cb3b31fe4a046c6011adbf5d4fcaec772398
saraelshaer <saraelshaer923@gmail.com>
2025-05-17 01:28:28 +0300

    Add TourService

---
+++ YallaGo.BLL/Services/TourService.cs
@@ -0,0 +1,179 @@
+﻿using YallaGo.BLL.DTOs.TourDtos;
+using YallaGo.BLL.Interfaces;
+using YallaGo.DAL.Models;
+using YallaGo.DAL.Repositories;
+
+namespace YallaGo.BLL.Services
+{
+    public class TourService : ITourService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TourService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<ReadTourDto> CreateTourAsync(TourDto tourDto)
+        {
+            var tour = new Tour
+            {
+                Name = tourDto.Name,
+                Description = tourDto.Description,
+                Price = tourDto.Price,
+                Duration = tourDto.Duration,
+                DestinationId = tourDto.DestinationId,
+                AvailableSeats = tourDto.AvailableSeats,
+            };
+            await _unitOfWork.TourRepo.AddAsync(tour);
+            await _unitOfWork.CompleteAsync();
+
+            return new ReadTourDto
+            {
+                Id = tour.Id,
+                Name = tour.Name,
+                Description = tour.Description,
+                Price = tour.Price,
+                Duration = tour.Duration,
+                DestinationId = tour.DestinationId,
+                AvailableSeats = tour.AvailableSeats,
+                ImageURL = tour.ImageURL
+            };
+        }
+
+        public async Task<bool> DeleteTourAsync(int id)
+        {
+            var tour = await _unitOfWork.TourRepo.GetByIdAsync(id);
+            if (tour == null)
+            {
+                return false;
+            }
+            _unitOfWork.TourRepo.HardDelete(tour);
+            await _unitOfWork.CompleteAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<ReadTourDto>> GetAllToursAsync()
+        {
+            var tours = await _unitOfWork.TourRepo.GetAllAsync();
+            var tourDtos = tours.Select(t => new ReadTourDto
+            {
+                Id = t.Id,
+                Name = t.Name,
+                Description = t.Description,
+                Price = t.Price,
+                Duration = t.Duration,
+                DestinationId = t.DestinationId,
+                AvailableSeats = t.AvailableSeats,
+                ImageURL = t.ImageURL
+            }).ToList();
+
+            return tourDtos;
+        }
+
+        public async Task<ReadTourDto> GetTourByIdAsync(int id)
+        {
+            var tour = await _unitOfWork.TourRepo.GetByIdAsync(id);
+            if (tour == null)
+            {
+                return null;
+            }
+            var tourDto = new ReadTourDto
+            {
+                Id = tour.Id,
+                Name = tour.Name,
+                Description = tour.Description,
+                Price = tour.Price,
+                Duration = tour.Duration,
+                DestinationId = tour.DestinationId,
+                AvailableSeats = tour.AvailableSeats,
+                ImageURL = tour.ImageURL
+            };
+            return tourDto;
+        }
+
+        public async Task<IEnumerable<ReadTourDto>> GetToursByDestinationIdAsync(int destinationId)
+        {
+            var tours = await _unitOfWork.TourRepo.GetAllAsync(t => t.DestinationId == destinationId);
+            var tourDtos = tours.Select(t => new ReadTourDto
+            {
+                Id = t.Id,
+                Name = t.Name,
+                Description = t.Description,
+                Price = t.Price,
+                Duration = t.Duration,
+                DestinationId = t.DestinationId,
+                AvailableSeats = t.AvailableSeats,
+                ImageURL = t.ImageURL
+            }).ToList();
+
+            return tourDtos;
+
+        }
+
+        public async Task<IEnumerable<ReadTourDto>> GetToursByPriceRangeAsync(decimal minPrice, decimal maxPrice)
+        {
+            var tours = await _unitOfWork.TourRepo.GetAllAsync(t => t.Price >= minPrice && t.Price <= maxPrice);
+            var tourDtos = tours.Select(t => new ReadTourDto
+            {
+                Id = t.Id,
+                Name = t.Name,
+                Description = t.Description,
+                Price = t.Price,
+                Duration = t.Duration,
+                DestinationId = t.DestinationId,
+                AvailableSeats = t.AvailableSeats,
+                ImageURL = t.ImageURL
+            }).ToList();
+
+            return tourDtos;
+        }
+
+        public async Task<IEnumerable<ReadTourDto>> SearchToursAsync(string searchTerm)
+        {
+            var tours = await _unitOfWork.TourRepo.GetAllAsync(t => t.Name.Contains(searchTerm));
+            var tourDtos = tours.Select(t => new ReadTourDto
+            {
+                Id = t.Id,
+                Name = t.Name,
+                Description = t.Description,
+                Price = t.Price,
+                Duration = t.Duration,
+                DestinationId = t.DestinationId,
+                AvailableSeats = t.AvailableSeats,
+                ImageURL = t.ImageURL
+            }).ToList();
+
+            return tourDtos;
+        }
+
+        public async Task<ReadTourDto> UpdateTourAsync(int id, TourDto tourDto)
+        {
+            var tour = await _unitOfWork.TourRepo.GetByIdAsync(id);
+            if (tour == null)
+            {
+                return null;
+            }
+            tour.Name = tourDto.Name;
+            tour.Description = tourDto.Description;
+            tour.Price = tourDto.Price;
+            tour.Duration = tourDto.Duration;
+            tour.DestinationId = tourDto.DestinationId;
+            tour.AvailableSeats = tourDto.AvailableSeats;
+            tour.ImageURL = tourDto.ImageURL;
+
+            await _unitOfWork.CompleteAsync();
+
+            return new ReadTourDto
+            {
+                Id = tour.Id,
+                Name = tour.Name,
+                Description = tour.Description,
+                Price = tour.Price,
+                Duration = tour.Duration,
+                DestinationId = tour.DestinationId,
+                AvailableSeats = tour.AvailableSeats,
+                ImageURL = tour.ImageURL
+            };
+        }
+    }
+}
