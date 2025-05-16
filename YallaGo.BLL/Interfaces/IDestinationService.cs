fc0a74d9ea93efffce5023cb289bf694847c88df
saraelshaer <saraelshaer923@gmail.com>
2025-05-17 00:02:48 +0300

    Add services layer

---
+++ YallaGo.BLL/Interfaces/IDestinationService.cs
@@ -0,0 +1,13 @@
+﻿using YallaGo.BLL.DTOs.DestinationDtos;
+
+namespace YallaGo.BLL.Interfaces
+{
+    public interface IDestinationService
+    {
+        Task<IEnumerable<ReadDestinationDto>> GetAllDestinationsAsync();
+        Task<ReadDestinationDto> GetDestinationByIdAsync(int id);
+        Task CreateDestinationAsync(DestinationDto destinationDto);
+        Task<ReadDestinationDto> UpdateDestinationAsync(int id, DestinationDto destinationDto);
+        Task<bool> DeleteDestinationAsync(int id);
+    }
+}
---
+++ YallaGo.BLL/Interfaces/ITourService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YallaGo.BLL.DTOs.TourDtos;
+
+namespace YallaGo.BLL.Interfaces
+{
+    public interface ITourService
+    {
+        Task<IEnumerable<TourDto>> GetAllToursAsync();
+        Task<TourDto> GetTourByIdAsync(int id);
+        Task<TourDto> CreateTourAsync(TourDto tourDto);
+        Task<TourDto> UpdateTourAsync(int id, TourDto tourDto);
+        Task<bool> DeleteTourAsync(int id);
+        Task<List<TourDto>> GetToursByDestinationIdAsync(int destinationId);
+        Task<List<TourDto>> GetToursByPriceRangeAsync(decimal minPrice, decimal maxPrice);
+        Task<List<TourDto>> SearchToursAsync(string searchTerm);
+    }
+}
